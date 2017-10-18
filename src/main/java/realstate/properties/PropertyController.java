dc9fde346752c65dcdc240171e1448ed9be22bd7
luckysher <luckyshera95@gmail.com>
2017-10-19 02:07:56 +0530

    updated property controller

--- src/main/java/realstate/properties/PropertyController.java
+++ src/main/java/realstate/properties/PropertyController.java
@@ -7,4 +7,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.BasicConfigurator;
 
-
+@Controller
+public class PropertyController {
+		
+	protected final Log logger = LogFactory.getLog(getClass());
+    @RequestMapping("property")
+    public ModelAndView getPropertyView(){       
+    	BasicConfigurator.configure();
+    	
+		logger.info("Property View controller calling..");
+        return new ModelAndView("propertyview", "house", "list of house from controller");
+    }
+}
