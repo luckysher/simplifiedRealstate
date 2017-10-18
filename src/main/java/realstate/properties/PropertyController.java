9f877f2c898fd6144d8a6606f756e0edcde099f9
luckysher <luckyshera95@gmail.com>
2017-10-19 02:07:26 +0530

    added property controller

--- src/main/java/realstate/properties/PropertyController.java
+++ src/main/java/realstate/properties/PropertyController.java
@@ -1,5 +1,10 @@
 package realstate.properties;
 
-public class PropertyController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
 
-}
+
