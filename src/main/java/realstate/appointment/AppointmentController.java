aaaf17123b825426eaf48f8f5cd439552581f217
luckysher <luckyshera95@gmail.com>
2017-10-27 12:13:27 +0530

    added controller and mapping for new  appointment

--- src/main/java/realstate/appointment/AppointmentController.java
+++ src/main/java/realstate/appointment/AppointmentController.java
@@ -1,5 +1,22 @@
 package realstate.appointment;
 
-public class AppointmentController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
+
+@Controller
+public class AppointmentController{
+
+	@RequestMapping(value="appointment/new", method=RequestMethod.GET)
+	public ModelAndView appointmentForm() throws Exception {
+			ModelAndView view = new ModelAndView();
+			view.setViewName("appointmentview");
+			view.addObject("command", new Appointment());
+		return view;
+	}
+	
 }
