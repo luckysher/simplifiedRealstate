c4be19ea5524d65af1f8a5409e0c81b82fecfc21
luckysher <luckyshera95@gmail.com>
2017-11-01 11:06:39 +0530

    added logger

--- src/main/java/realstate/appointment/AppointmentController.java
+++ src/main/java/realstate/appointment/AppointmentController.java
@@ -1,16 +1,25 @@
 package realstate.appointment;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
 
 
 @Controller
 public class AppointmentController{
-
+	protected final Log logger = LogFactory.getLog(getClass());
+	public AppointmentController(){
+		BasicConfigurator.configure();
+	}
+	
 	@RequestMapping(value="appointment/new", method=RequestMethod.GET)
 	public ModelAndView appointmentForm() throws Exception {
 			ModelAndView view = new ModelAndView();
@@ -20,11 +29,11 @@ public class AppointmentController{
 	}
 	@RequestMapping(value="appointment/save", method=RequestMethod.POST)
 	public ModelAndView appointmentSave(@ModelAttribute("")Appointment appointment, ModelMap model) {
+			
 			model.addAttribute("name", appointment.getName());
 			model.addAttribute("address", appointment.getAddress());
-			
 			ModelAndView view = new ModelAndView();
-			view.setViewName("appointment_success");
+			view.setViewName("appointment_success");			
 			view.addAllObjects(model);
 			
 		return view;
