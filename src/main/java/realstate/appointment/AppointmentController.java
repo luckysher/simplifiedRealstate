cde2849045bf8d241d78d55d88b60767e2c9854d
luckysher <luckyshera95@gmail.com>
2017-11-07 14:03:14 +0530

    added import for custom validation

--- src/main/java/realstate/appointment/AppointmentController.java
+++ src/main/java/realstate/appointment/AppointmentController.java
@@ -1,21 +1,33 @@
 package realstate.appointment;
 
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.BasicConfigurator;
+import service.AppointmentManager;
+import form.validator.AppointmentFormValidator;
+
 
 
 @Controller
+//@SessionAttributes("appointment")
 public class AppointmentController{
 	protected final Log logger = LogFactory.getLog(getClass());
+	 
+	
 	public AppointmentController(){
 		BasicConfigurator.configure();
 	}
@@ -24,12 +36,13 @@ public class AppointmentController{
 	public ModelAndView appointmentForm() throws Exception {
 			ModelAndView view = new ModelAndView();
 			view.setViewName("appointmentview");
-			view.addObject("command", new Appointment());
+			view.addObject("appointment", new Appointment());
 		return view;
 	}
 	@RequestMapping(value="appointment/save", method=RequestMethod.POST)
-	public ModelAndView appointmentSave(@ModelAttribute("")Appointment appointment, ModelMap model) {
-			
+	public ModelAndView appointmentSave(@ModelAttribute("appointment")@Valid Appointment appointment, BindingResult bindingResult, ModelMap model) {
+		
+	
 			model.addAttribute("name", appointment.getName());
 			model.addAttribute("address", appointment.getAddress());
 			ModelAndView view = new ModelAndView();
