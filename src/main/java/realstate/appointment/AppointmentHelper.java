487adbe93fb7e74fce1b95bfa0b9633f0da45ba4
luckysher <luckyshera95@gmail.com>
2017-11-20 12:07:03 +0530

    addded appointment helper class

---
+++ src/main/java/realstate/appointment/AppointmentHelper.java
@@ -0,0 +1,13 @@
+package realstate.appointment;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import java.net.URLClassLoader;
+import java.net.URL;
+
+public class AppointmentHelper {
+	
+
+}
