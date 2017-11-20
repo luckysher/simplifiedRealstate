ad17b82413c886aabd20899b3489dc6f1b5fde70
luckysher <luckyshera95@gmail.com>
2017-11-20 12:08:56 +0530

    added fun for getting dao in appointment helper

--- src/main/java/realstate/appointment/AppointmentHelper.java
+++ src/main/java/realstate/appointment/AppointmentHelper.java
@@ -12,6 +12,21 @@ public class AppointmentHelper {
 		System.out.println("initializing appointment helper");
 	}
 	
-	
+	public Object getRealstateDao(String daoBeanName){
+		Object dao = null;
+		 ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+	     URL[] urls = ((URLClassLoader)cl).getURLs();
+
+	     for(URL url: urls){
+	        	System.out.println(url.getFile());
+	        }
+		
+		String contextFilePath = "/applicationContext.xml";		
+		Resource r = new ClassPathResource(contextFilePath);
+		BeanFactory beanfactory = new XmlBeanFactory(r);
+		dao = beanfactory.getBean("d");
+		return dao;
+	}
 
 }
