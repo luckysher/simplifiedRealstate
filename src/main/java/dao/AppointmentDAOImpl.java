84cb3697714c36fa0778524d60c6f5bfb2594cd7
luckysher <luckyshera95@gmail.com>
2017-11-21 17:31:14 +0530

    added dao impl for appointment

--- src/main/java/dao/AppointmentDAOImpl.java
+++ src/main/java/dao/AppointmentDAOImpl.java
@@ -2,27 +2,38 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import models.Appointment;
+import org.springframework.orm.hibernate3.HibernateTemplate;  
 import org.springframework.stereotype.Repository;
-
-import realstate.appointment.Appointment;
 
 @Repository
 public class AppointmentDAOImpl implements AppointmentDAO {
-
-	public List<Appointment> getAllAppointments() 
-	{
-		List<Appointment> appointments = new ArrayList<Appointment>();
-		
-		Appointment appt = new Appointment();
-		appt.setName("sher");
-		appt.setAddress("Rasulpur khurd");
-		appt.setArea("kernal");
-		appt.setMobileNum((long)989677880);
-		appt.setDetails("Phoosgrarh");
-
-		appointments.add(appt);
-		
-		return appointments;
+	
+	private HibernateTemplate template;  
+	public void setTemplate(HibernateTemplate template) {  
+	    this.template = template;  
+	}  
+	//method to save appointment
+	public void saveAppointment(Appointment a){  
+	    template.save(a);  
+	}  
+	//method to update Appointment
+	public void updateAppointment(Appointment a){  
+	    template.update(a);  
+	}  
+	//method to delete appointment
+	public void deleteAppointment(Appointment a){  
+	    template.delete(a);  
+	}  
+	//method to return one appointment of given id  
+	public Appointment getById(int id){  
+	    Appointment a=(Appointment)template.get(Appointment.class,id);  
+	    return a;  
+	}  
+	//method to return all appointments
+	public List<Appointment> getAppointments(){  
+	    List<Appointment> list=new ArrayList<Appointment>();  
+	    list=template.loadAll(Appointment.class);  
+	    return list;  
 	}
-}
+}  
