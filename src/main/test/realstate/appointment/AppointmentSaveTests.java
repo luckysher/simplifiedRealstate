97a29124b0860d0688d7cbd754c809a0158758e7
luckysher <luckyshera95@gmail.com>
2017-11-21 17:46:45 +0530

    added imports for appointment test

---
+++ src/main/test/realstate/appointment/AppointmentSaveTests.java
@@ -0,0 +1,16 @@
+
+package realstate.appointment;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.List;
+import javax.annotation.Resource;
+import dao.AppointmentDAOImpl;
+import models.Appointment;
+
