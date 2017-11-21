543f21be410c29c7309993ea2599a6287580ea45
luckysher <luckyshera95@gmail.com>
2017-11-21 17:18:31 +0530

    added import for model

--- src/main/java/form/validator/AppointmentFormValidator.java
+++ src/main/java/form/validator/AppointmentFormValidator.java
@@ -1,6 +1,8 @@
 package form.validator;
 
 
+import models.Appointment;
+
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.stereotype.Component;
@@ -8,8 +10,6 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 import org.apache.commons.logging.Log;
-
-import realstate.appointment.*;
 
 @Component
 public class AppointmentFormValidator implements Validator{
